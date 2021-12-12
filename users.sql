6daa0f7a167a2c06c94daa1f21a25738058f320f
mohitsingh2006 <mohitsingh2006@gmail.com>
2021-12-12 19:59:06 +0530

    project assets

---
+++ users.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `users` (`id`, `name`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'Constance Noring', 'noring@example.com', '2021-12-12 14:12:14', NULL),
+(2, 'Isabelle Ringing', 'ringing@example.com', '2021-12-12 14:12:14', NULL),
+(3, 'Turner', 'Turner@example.com', '2021-12-12 14:12:14', NULL),
+(4, 'Anthemum', 'Anthemum@example.com', '2021-12-12 14:12:14', NULL),
+(5, 'Anita ', 'Anita@example.com', '2021-12-12 14:12:14', NULL),
+(6, 'Amanda ', 'Amanda@example.com', '2021-12-12 14:12:14', NULL),
+(7, 'Loco ', 'Loco@example.com', '2021-12-12 14:12:14', NULL),
+(8, 'Con ', 'Con@example.com', '2021-12-12 14:12:14', NULL),
+(9, 'Lana ', 'Lana@example.com', '2021-12-12 14:12:14', NULL),
+(10, 'Barry ', 'Barry@example.com', '2021-12-12 14:12:14', NULL),
+(11, 'Marsha ', 'Marsha@example.com', '2021-12-12 14:12:14', NULL),
+(12, 'Zeimers', 'Zeimers@example.com', '2021-12-12 14:12:14', NULL),
+(13, 'Allysis', 'Allysis@example.com', '2021-12-12 14:12:14', NULL),
+(14, 'Lois ', 'Lois@example.com', '2021-12-12 14:12:14', NULL),
+(15, 'Clark ', 'Clark@example.com', '2021-12-12 14:12:14', NULL),
+(16, 'Joseph ', 'Joseph@example.com', '2021-12-12 14:12:14', NULL),
+(17, 'Simon ', 'Simon@example.com', '2021-12-12 14:12:14', NULL),
+(18, 'Dixie ', 'Dixie@example.com', '2021-12-12 14:12:14', NULL),
+(19, 'Felicity ', 'Felicity@example.com', '2021-12-12 14:12:14', NULL),
+(20, 'Tiffany ', 'Tiffany@example.com', '2021-12-12 14:12:14', NULL),
+(21, 'Shady', 'Shady@example.com', '2021-12-12 14:14:33', NULL),
+(22, 'Mundy', 'Mundy@example.com', '2021-12-12 14:14:33', NULL),
+(23, 'Des E', 'DesE@example.com', '2021-12-12 14:14:33', NULL),
+(24, 'Pete ', 'Pete@example.com', '2021-12-12 14:14:33', NULL),
+(25, 'Kissy', 'Kissy@example.com', '2021-12-12 14:14:33', NULL),
+(26, 'Chew', 'Chew@example.com', '2021-12-12 14:14:33', NULL),
+(27, 'Ruby', 'Ruby@example.com', '2021-12-12 14:14:33', NULL),
+(28, 'May', 'May@example.com', '2021-12-12 14:14:33', NULL);
